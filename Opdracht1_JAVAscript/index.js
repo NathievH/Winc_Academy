218522a22c96cfaddac21fc40a5c47c408805ab5
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-21 11:56:31 +0800

    Add files via upload

--- Opdracht1_JAVAscript/index.js
+++ Opdracht1_JAVAscript/index.js
@@ -1,27 +1,25 @@
 console.log("Hello Winc Academy");
 let name = "nathalie";
-
-
 console.log(name);
-
+//je kunt geen variable maken met de naam if. 
+let sum = 3 + 4;
+console.log("sum:", sum);
+let sumwithstrings = "3" + "4";
+console.log("sum with strings:", sumwithstrings);
+let multiplication = 100 * 200;
+console.log("multiplication:", multiplication);
+let division = 50 / 5;
+console.log("division;", division);
+let modulus = 100 % 10;
+console.log("modulus:", modulus);
 let age = 28;
-
-let person = {
-    name:'Nathalie',
-    age: '28'
-}
-console.log(person);
+console.log(typeof age);
+let ageasstring ="28";
+console.log(typeof ageasstring);
 
 
-2+2
-"2"+"2"
-
-y=5
-x=y+2
 
 
-addition 
-substraction
 
 
 
