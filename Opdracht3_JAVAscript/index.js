6e0cfe8d0badf4abf170c43b9f56685325f489be
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-21 15:31:54 +0800

    Add files via upload

---
+++ Opdracht3_JAVAscript/index.js
@@ -0,0 +1,27 @@
+var age = 25;
+if (age > 18) {
+    console.log("You are allowed to enter");
+}
+else if (age == 18) {
+    console.log("Lucky you, you old enough to enter");
+}
+else if (age < 18) {
+    console.log("You are NOT old enough to enter, please wait outside");
+}
+
+var Female = true;
+if (Female) {
+    console.log(" Enjoy LADIES night");
+}
+else {
+    console.log("Its ladies night sorry you van not enter");
+}
+
+var driverStatus = Bob;
+if (Bob) {
+    console.log("Je mag rijden");
+}
+else {
+    console.log("Je mag niet meer rijden");
+}
+
