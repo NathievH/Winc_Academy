5c20564df3f59bed6f563c5412e487736fe41981
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-21 14:15:22 +0800

    Add files via upload

---
+++ Opdracht2_JAVAscript/index.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy Studenten");
+/* 
+let multiplication = 1230941 * 1823794;
+console.log ("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:",division);
+*/
+
+
+
