a2d6825cfdad38243bd55d1dcfca5cf1cac4d829
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-21 11:41:34 +0800

    Add files via upload

---
+++ Opdracht1_JAVAscript/index.js
@@ -0,0 +1,27 @@
+console.log("Hello Winc Academy");
+let name = "nathalie";
+
+
+console.log(name);
+
+let age = 28;
+
+let person = {
+    name:'Nathalie',
+    age: '28'
+}
+console.log(person);
+
+
+2+2
+"2"+"2"
+
+y=5
+x=y+2
+
+
+addition 
+substraction
+
+
+
