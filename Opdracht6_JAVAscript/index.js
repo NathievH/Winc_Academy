34ff7fd20b3b7c555823bb09c0484814d734c6a0
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-24 17:30:10 +0800

    Add files via upload

---
+++ Opdracht6_JAVAscript/index.js
@@ -0,0 +1,24 @@
+const makingpizza = function () {
+    console.log ('verzamel je ingredienten')
+    console.log ('Maak je pizza deeg')
+    console.log ('rol je pizza deeg uit')
+    console.log ('versier je pizza met de ingredienten')
+    console.log ('Leg de pizza 15 min op 200* in de oven')
+}
+
+makingpizza (); 
+//makingpizza (); 
+//makingpizza (); 
+
+const bestsushi = function () {
+    console.log ('Cook the sushi rice')
+    console.log ('cut the fresh fish and vegtables')
+    console.log ('take the rollertoys')
+    console.log ('but a layer of rice fish and vegtable')
+    console.log ('rol the sushi en cut it in pieces')
+}
+
+bestsushi ();
+bestsushi ();
+bestsushi ();
+
