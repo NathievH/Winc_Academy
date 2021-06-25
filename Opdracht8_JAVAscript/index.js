09f3d02b56571732ff7a27a2f40742233b7d39fb
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-25 11:19:36 +0800

    Add files via upload

---
+++ Opdracht8_JAVAscript/index.js
@@ -0,0 +1,56 @@
+//Checking if a number is big 
+const check = function (number) {
+    if (number > 100) {
+        console.log("true")
+    }
+    else (number < 100)
+    { console.log("false") }
+
+    return check;
+
+}
+
+const number = 150;
+
+check();
+
+//bouncer at the club 
+
+const total = 250
+const age = 25
+
+const brendamaximum = function () {
+
+    if (age < 18) {
+        console.log("this club is only for adults")
+    }
+    else (age >= 18)
+    {
+        console.log("you are old enough to enter lets see how busy it is")
+    }
+
+    if (total < 250) {
+        console.log("you can come in")
+    }
+    else if (total > 249)
+    {
+        console.log("its too busy come back later")
+    }
+    return age;
+}
+
+brendamaximum();
+
+// calculating the avarage 
+
+const average = function (nbr1,nbr2,nbr3,nbr4,nbr5) {
+    sum = ( nbr1 + nbr2 + nbr3 + nbr4 + nbr5 / 5 );
+    console.log (sum);
+    return sum
+}
+
+average (8,8,8,8,8); 
+Math.round (sum) = sum
+console.log (sum)
+
+
