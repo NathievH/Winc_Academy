06d5b3b5f392ee03f13af949984774b9081b994d
NathievH <84772901+NathievH@users.noreply.github.com>
2021-06-28 22:04:22 +0800

    Add files via upload

---
+++ Opdracht12_JAVAscript/index.js
@@ -0,0 +1,21 @@
+//oude stijl loops 
+const Colors = ["yellow", "blue", "red", "orange"];
+
+let i = 0;
+while (i < Colors.length) {
+    console.log(Colors[i]);
+    i++;
+}
+
+
+for (let i = 0; i < Colors.length; i++) {
+    console.log (Colors[i]) ;
+}
+
+Colors.forEach 
+// 3 en 5.
+// 1
+// Het neemt niet veel regels in beslag, is overzichtelijker en je maakt minder snel foute omdat het een korte code is.
+// Dit weet ik niet.
+
+
